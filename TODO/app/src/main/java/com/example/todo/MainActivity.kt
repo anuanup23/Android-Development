cd8756fd7cb394989014792d9b1904b96b8f67dc
Anup Kumar <anuanupkumar0@gmail.com>
2021-01-07 10:01:25 +0530

    Adding TODO APP Project

---
+++ TODO/app/src/main/java/com/example/todo/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.todo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var toolbar: Toolbar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        toolbar  = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.history -> {
+                startActivity(Intent(this,HistoryActivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun openNewTask(view: View) {
+        startActivity(Intent(this, TaskActivity::class.java))
+    }
+}
---
+++ TODO/app/src/main/java/com/example/todo/TodoDao.kt
@@ -0,0 +1,18 @@
+package com.example.todo
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface TodoDao {
+    @Insert()
+    suspend fun insertTask(todoModel: TodoModel): Long
+    @Query("select * from TodoModel where isFinished != -1")
+    fun getTask (): LiveData<List<TodoModel>>
+    @Query("Update TodoModel Set isFinished = 1 where id = :uid")
+    fun finishTask (uid: Long)
+    @Query("Delete from  TodoModel where id = :uid")
+    fun deleteTask (uid: Long)
+}
